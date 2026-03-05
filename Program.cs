43fdd79a8720b91a9cf52c61b0fb145f18512c98
Vignesh K <Vignesh.K@Tekosol.net>
2026-03-05 15:18:20 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello from CI/CD Pipeline!");
+        Console.WriteLine($"Machine: {Environment.MachineName}");
+        Console.WriteLine($"Time: {DateTime.Now}");
+    }
+}
